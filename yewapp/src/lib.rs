87d0a60cccaa09ee99aafc56f00b22f666d2bdda
maxpetts <maxpetts9@gmail.com>
2022-06-03 12:38:09 +0100

    Add router

--- yewapp/src/lib.rs
+++ yewapp/src/lib.rs
@@ -4,6 +4,9 @@ use yew_router::prelude::*;
 #[function_component(App)]
 pub fn app() -> Html {
     html! {
+        <BrowserRouter>
+            <Switch<Route> render={Switch::render(switch)} />
+        </BrowserRouter>
     }
 }
 mod components;
