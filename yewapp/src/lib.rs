d7f9ab8ba0c9d558e11e119bcf0e320c8b5488a0
maxpetts <maxpetts9@gmail.com>
2022-06-03 12:39:51 +0100

    Add hash using persistent session store

--- yewapp/src/lib.rs
+++ yewapp/src/lib.rs
@@ -1,18 +1,41 @@
+mod components;
+mod pages;
+mod router;
+
+use gloo::console::log;
+use rand::{distributions::Alphanumeric, Rng};
+use router::{switch, Route};
 use yew::prelude::*;
 use yew_router::prelude::*;
+use yewdux::prelude::*;
+use yewdux_functional::use_store;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Default, Clone, Deserialize, Serialize)]
+struct State {
+    hash: Option<String>,
+}
+
+impl Persistent for State {
+    fn area() -> Area {
+        Area::Session
+    }
+}
 
 #[function_component(App)]
 pub fn app() -> Html {
+    let store = use_store::<PersistentStore<State>>();
+    let hash = store
+        .state()
+        .map(|state| state.hash.as_ref())
+        .unwrap_or_default();
+
+
     html! {
         <BrowserRouter>
             <Switch<Route> render={Switch::render(switch)} />
+            <p>{format!{"{:#?}", hash}}</p>
         </BrowserRouter>
     }
 }
-mod components;
-mod pages;
-mod router;
-
-mod components;
-mod pages;
-mod router;
