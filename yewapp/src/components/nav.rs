dac1b83853c86190f05859a104e1bd897351250c
Max Petts <maxpetts9@gmail.com>
2023-01-12 16:19:39 +0000

    - Move login into nav bar - Remove the old&odd component call

--- yewapp/src/components/nav.rs
+++ yewapp/src/components/nav.rs
@@ -1,20 +1,17 @@
 use yew::prelude::*;
 use yew_router::prelude::*;
 
-use crate::router::Route;
+use crate::{components::login::Login, router::Route};
 
 #[function_component(Nav)]
 pub fn nav() -> Html {
     let navigator = use_navigator().unwrap();
 
-    let profile_button = {
-        let oncliche = Callback::from(move |_| navigator.push(&Route::Profile));
-
-        html! {
+    let oncliche = Callback::from(move |_| navigator.push(&Route::Profile));
+    html!(
+        <>
             <button onclick={oncliche}>{"Profile"}</button>
-        }
-    };
-    profile_button
-
-    // html! {}
+            <Login />
+        </>
+    )
 }
